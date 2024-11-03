fc67a814db74f0dca244a06c21680c4ce8f6230a
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-11-04 02:41:57 +0800

    feat: pub all the funcs of ZZX

--- src/util.rs
+++ src/util.rs
@@ -7,30 +7,30 @@ pub struct ZZX {
 }
 
 impl ZZX {
-    fn new() -> Self {
+    pub fn new() -> Self {
         ZZX { coeffs: vec![] }
     }
 
-    fn new_with_u64(n: u64) -> Self {
+    pub fn new_with_u64(n: u64) -> Self {
         ZZX { coeffs: vec![Integer::from(n)] }
     }
 
-    fn new_with_integer(n: Integer) -> Self {
+    pub fn new_with_integer(n: Integer) -> Self {
         ZZX { coeffs: vec![n] }
     }
 
     /*************************
      * Some utility functions
      *************************/
-    fn set_coeff<T>(&mut self, i: usize, n: Option<T>) where T: Into<Integer> {
+    pub fn set_coeff<T>(&mut self, i: usize, n: Option<T>) where T: Into<Integer> {
         self.coeffs[i] = n.map(Into::into).unwrap_or(Integer::from(1));
     }
 
-    fn deg(&self) -> i64 {
+    pub fn deg(&self) -> i64 {
         self.coeffs.len() as i64 - 1
     }
 
-    fn coeff(&self, i: usize) -> Integer {
+    pub fn coeff(&self, i: usize) -> Integer {
         if i >= self.coeffs.len() {
             Integer::from(0)
         } else {
@@ -38,7 +38,7 @@ impl ZZX {
         }
     }
 
-    fn get_coeff(&self, i: usize) -> Integer {
+    pub fn get_coeff(&self, i: usize) -> Integer {
         if i >= self.coeffs.len() {
             Integer::from(0)
         } else {
