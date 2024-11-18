82af015e948aa8a3bce3b4645c32d9f514a233d7
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-11-18 16:22:44 +0800

    feat: add some params

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,21 @@ mod encryption_scheme;
 mod sampling;
 mod util;
 
+// // RLWE
+// const P: usize = 1024;
+// const Q: usize = 11289;
+// const SIGMA: f32 = 3.19;
+
+// // ALTERNATE
+// const P: usize = 14; // poly degree
+// const Q: usize = 179424673; // 15485863, 8380417
+// const SIGMA: f32 = 2.0;
+
+// NTRU: NTRU Prime parameters
+const P: usize = 761;
+const Q: usize = 4591;
+const SIGMA: f32 = 2.0;
+
 fn main() {
     println!("Hello, world!");
 }
