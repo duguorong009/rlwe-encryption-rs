d6807bf43229669efce2e33b2d2fae80c61a58b5
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-11-27 15:14:49 +0800

    feat: create & impl "ZZ_pContext"

--- src/util/zz_p.rs
+++ src/util/zz_p.rs
@@ -1,1 +1,17 @@
+use rug::Integer as ZZ;
 
+#[derive(Debug, Clone)]
+struct ZZ_pContext {
+    p: ZZ,
+}
+
+impl ZZ_pContext {
+    fn new(p: ZZ) -> Self {
+        assert!(p > 1, "ZZ_pContext: p must be > 1");
+        Self { p }
+    }
+
+    pub fn modulus(&self) -> &ZZ {
+        &self.p
+    }
+}
