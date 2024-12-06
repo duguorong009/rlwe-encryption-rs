09c64b058802ffae890883eb58bd990e8eb2b135
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-12-06 17:37:10 +0800

    chore: comment out unused func

--- src/sampling.rs
+++ src/sampling.rs
@@ -164,29 +164,29 @@ impl Sampling {
         over_s * tmp4
     }
 
-    //  Method for computing the binary expansion of a given probability in [0, 1]
-    fn binary_expansion(
-        &self,
-        aux_p: &mut Vec<Vec<i32>>,
-        mut probability: Float,
-        precision: u64,
-        index: usize,
-    ) {
-        let mut i = -1;
-        let mut j: usize = 0;
+    // //  Method for computing the binary expansion of a given probability in [0, 1]
+    // fn binary_expansion(
+    //     &self,
+    //     aux_p: &mut Vec<Vec<i32>>,
+    //     mut probability: Float,
+    //     precision: u64,
+    //     index: usize,
+    // ) {
+    //     let mut i = -1;
+    //     let mut j: usize = 0;
 
-        while probability > 0 && ((j as u64) < precision) {
-            let pow = Float::with_val(self.precision, 2).pow(i); // 2 ^ {i}
-            if pow <= probability {
-                aux_p[j][index] = 1;
-                probability -= pow;
-            } else {
-                aux_p[j][index] = 0;
-            }
-            i -= 1;
-            j += 1;
-        }
-    }
+    //     while probability > 0 && ((j as u64) < precision) {
+    //         let pow = Float::with_val(self.precision, 2).pow(i); // 2 ^ {i}
+    //         if pow <= probability {
+    //             aux_p[j][index] = 1;
+    //             probability -= pow;
+    //         } else {
+    //             aux_p[j][index] = 0;
+    //         }
+    //         i -= 1;
+    //         j += 1;
+    //     }
+    // }
 }
 
 // bit = 0 then return a
